6c6613c7aa718e5ced9f0c1a08b93f26d35d5a77
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2023-11-07 10:58:50 -0500

    add remove space

---
+++ remove_space/README.md
@@ -0,0 +1,4 @@
+forma de ejecutar el archivo
+
+en la consola :
+node <name_file.js>
---
+++ remove_space/read_write_file.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+
+fs.readFile("data.txt", (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  let arrayData = data.toString().split("\n");
+  console.log(arrayData);
+  // console.log(arrayData.replace(' ', '_'))
+  let newArray = arrayData.map((item) => {
+	
+		let data = item.trim().replaceAll(" ", "_").toLowerCase();
+				return data+="\n"
+	})
+  console.log(newArray);
+  console.log("============================");
+  console.log("============================");
+  console.log("============================");
+  console.log("============================");
+  console.log("============================");
+
+  const result = newArray.reduce((acc, item) => {
+    return acc+=item;
+  }, "");
+
+
+
+console.log("-----------result")
+console.log(result)
+console.log(result)
+console.log(result)
+console.log(result)
+console.log(result)
+console.log(result)
+
+
+  data = result
+	const fileName = "data.txt"
+
+fs.writeFile(fileName,data,(error)=>{
+if(error){
+	console.error("error writing to the file",error)
+	return 
+}
+console.log("file `${fileName}` has been created with the data ")
+})
+
+   
+});
